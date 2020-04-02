cda7bc8b325d51c2c892bc59049baad03508dd83
KG <kg.vitaty14@gmail.com>
2020-04-02 18:08:22 +0900

    Initial Commit

---
+++ locationSunrise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  locationSunrise
+//
+//  Created by Kouga Hoshi on 2020/04/02.
+//  Copyright © 2020 KG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
